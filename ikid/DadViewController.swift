9df9419257fd8a656be45b52df17cc3194ececc8
Lisa Koss <lisakoss@uw.edu>
2018-02-05 10:28:29 -0800

    Redo structure + use simple UIView transition func instead

--- ikid/DadViewController.swift
+++ ikid/DadViewController.swift
@@ -1,44 +1,35 @@
 //
-//  GoodViewController.swift
+//  DadViewController.swift
 //  ikid
 //
-//  Created by Lisa Koss on 2/4/18.
+//  Created by Lisa Koss on 2/5/18.
 //  Copyright © 2018 Lisa Koss. All rights reserved.
 //
 
 import UIKit
 
-class GoodViewController: UIViewController {
+class DadViewController: UIViewController {
     
-    var showAnswer = false
-    var count = 1
-    @IBOutlet weak var button: UIButton!
+    var showAnswer = true
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     
     @IBAction func next(_ sender: Any) {
         if(showAnswer) {
-            UIView.transition(with: self.view, duration: 0.5, options: .transitionFlipFromLeft, animations: {}, completion: nil)
+            UIView.transition(with: self.view, duration: 0.5, options: .transitionFlipFromRight, animations: {}, completion: nil)
             
-            label.text = "No, I prefer Google."
+            label.text = "They haven't got a gig yet."
             button.setTitle("Question", for: .normal)
             showAnswer = false
         } else if(!showAnswer) {
-            UIView.transition(with: self.view, duration: 0.5, options: .transitionFlipFromRight, animations: {}, completion: nil)
-            if(count == 1) {
-                label.text = "Who's there?"
-                button.setTitle("Next", for: .normal)
-                count += 1
-            } else if(count == 2) {
-                label.text = "Yah?"
-                count += 1
-            } else if(count == 3) {
-                label.text = "Yah who?"
-                showAnswer = true
-                count = 1
-            }
+            UIView.transition(with: self.view, duration: 0.5, options: .transitionFlipFromLeft, animations: {}, completion: nil)
+            
+            label.text = "Have you heard of the band 1023mb?"
+            button.setTitle("Punchline", for: .normal)
+            showAnswer = true
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
